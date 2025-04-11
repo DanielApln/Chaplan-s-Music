52c68a7838b997d1b7aa881f797ae16b4f1974c1
DanielApln <118504443+DanielApln@users.noreply.github.com>
2025-04-11 13:08:35 +0800

    1.0.3
    product slider

--- Script.js
+++ Script.js
@@ -1,3 +1,20 @@
+// Second slider
+const productContainersSecond = [...document.querySelectorAll('.product-container-second')];
+const nxtBtnSecond = [...document.querySelectorAll('.nxt-btn-second')];
+const preBtnSecond = [...document.querySelectorAll('.pre-btn-second')];
+
+productContainersSecond.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtnSecond[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtnSecond[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})// First slider
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
