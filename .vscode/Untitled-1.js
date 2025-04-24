47ac15f1967e1f44f2fd5ee7ead6f7556f965be7
kakyoin2133 <chuajohnpaulrubia@gmail.com>
2025-04-24 16:06:56 +0800

    GIF for Feature sa Home
    Gifs

---
+++ .vscode/Untitled-1.js
@@ -0,0 +1,15 @@
+const firstnum = String("1234567890");
+
+const secondnum = Number (firstnum);
+
+const thirdnum = BigInt(secondnum);
+
+const fourthnum = Boolean(thirdnum);
+
+count 
+
+
+
+console.log(fourthnum); // "Script"
+console.log(typeof fourthnum);
+console.log(numb );
